2f5c182e80ee1baa2c509af551561787513ac04f
Theforcebemay <kubacortin@gmail.com>
2016-06-17 19:37:21 +0300

    4 stage done, authorisation

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
-from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from snippets.models import Snippet  # LANGUAGE_CHOICES, STYLE_CHOICES
+
 
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
-
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner')
 
     def create(self, validated_data):
         return Snippet.objects.create(**validated_data)
@@ -20,4 +21,14 @@ class SnippetSerializer(serializers.ModelSerializer):
         instance.save()
 
         return instance
+
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
 
--- snippets/views.py
+++ snippets/views.py
@@ -1,17 +1,38 @@
 # -*- coding: utf-8 -*-
 from rest_framework import generics
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
+from django.contrib.auth.models import User
+from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
 # Create your views here.
+
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          )
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly)
 
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 # class SnippetList(mixins.ListModelMixin,
 #                   mixins.CreateModelMixin,
 #                   generics.GenericAPIView):
